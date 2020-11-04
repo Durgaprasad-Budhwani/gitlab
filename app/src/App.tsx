145e34722104881fb64d69a3949c8d76826cbe07
Matthew Congrove <mcongrove@pinpoint.com>
2020-11-04 11:44:29 -0600

    Update integration logos

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -11,10 +11,10 @@ function App() {
 			tags: ['Source Code Management', 'Issue Management'],
 			installed: false,
 			refType: 'gitlab',
-			icon: `data:image/svg+xml,%3Csvg aria-hidden='true' focusable='false' data-prefix='fab' data-icon='gitlab' class='svg-inline--fa fa-gitlab fa-w-16' role='img' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 512 512'%3E%3Cpath fill='currentColor' d='M22.2 32A16 16 0 0 0 6 47.8a26.35 26.35 0 0 0 .2 2.8l67.9 412.1a21.77 21.77 0 0 0 21.3 18.2h325.7a16 16 0 0 0 16-13.4L505 50.7a16 16 0 0 0-13.2-18.3 24.58 24.58 0 0 0-2.8-.2L22.2 32zm285.9 297.8h-104l-28.1-147h157.3l-25.2 147z'%3E%3C/path%3E%3C/svg%3E`,
+			icon: 'https://pinpoint.com/images/integrations/GitLab.svg',
 			publisher: {
 				name: 'Pinpoint',
-				avatar: 'https://avatars0.githubusercontent.com/u/24400526?s=200&v=4',
+				avatar: 'https://pinpoint.com/logo/logomark/blue.png',
 				url: 'https://pinpoint.com'
 			},
 			uiURL: window.location.href
